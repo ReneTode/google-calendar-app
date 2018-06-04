6b6fd21a331aaad4e8ebed700eef4239d9b47c46
Rene Tode <github@reot.org>
2018-06-04 21:36:50 +0200

    Add files via upload

---
+++ google-calendar-app/start_calendar.py
@@ -0,0 +1,33 @@
+"""
+Shows basic usage of the Google Calendar API. Creates a Google Calendar API
+service object and outputs a list of the next 10 events on the user's calendar.
+"""
+from __future__ import print_function
+from apiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+import datetime
+
+
+# Setup the Calendar API
+SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+store = file.Storage('credentials.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('/mnt/usbdrive/pi/appdaemon/appdaemon/conf/apps/testapps/client_secret.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+service = build('calendar', 'v3', http=creds.authorize(Http()))
+
+# Call the Calendar API
+now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+print('Getting the upcoming 10 events')
+events_result = service.events().list(calendarId='primary', timeMin=now,
+                                      maxResults=10, singleEvents=True,
+                                      orderBy='startTime').execute()
+events = events_result.get('items', [])
+
+if not events:
+    print('No upcoming events found.')
+for event in events:
+    start = event['start'].get('dateTime', event['start'].get('date'))
+    print(start, event['summary'])
