9c2ea2bf340cfea1c870950c306fe20d6c3a05b5
Rene Tode <github@reot.org>
2018-06-04 21:38:22 +0200

    Update start_calendar.py

--- google-calendar-app/start_calendar.py
+++ google-calendar-app/start_calendar.py
@@ -14,7 +14,7 @@ SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
 store = file.Storage('credentials.json')
 creds = store.get()
 if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('/mnt/usbdrive/pi/appdaemon/appdaemon/conf/apps/testapps/client_secret.json', SCOPES)
+    flow = client.flow_from_clientsecrets('/SET/YOUR/PATH/HERE/client_secret.json', SCOPES)
     creds = tools.run_flow(flow, store)
 service = build('calendar', 'v3', http=creds.authorize(Http()))
 
